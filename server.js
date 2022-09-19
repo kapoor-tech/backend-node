67c35aaa00700b3fef4c8bd33298efd1a382f845
Ashish Kapoor <air@dev.local>
2022-09-19 11:39:37 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+require("rootpath")();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const errorHandler = require("_middleware/error-handler");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
+
+// api routes
+app.use("/users", require("./users/user.controller"));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port =
+  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
+app.listen(port, () => console.log("Server listening on port " + port));
